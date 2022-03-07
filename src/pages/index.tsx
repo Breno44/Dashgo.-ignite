9c85ef50a1c9a06802ebf0a1bd92f165f83ef3fc
Breno <bhfs.2908@gmail.com>
2022-03-07 09:42:42 -0300

    feat: finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "../components/Form/Input";
-import Router from "next/router";
 
-export default function Home() {
-  function handleLogin(e) {
-    e.preventDefault();
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
-    Router.push("/dashboard");
-  }
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
 
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
@@ -19,11 +36,24 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register("email")}
+          />
 
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            error={errors.password}
+            {...register("password")}
+          />
         </Stack>
 
         <Button
@@ -31,7 +61,7 @@ export default function Home() {
           mt="6"
           colorScheme="pink"
           size="lg"
-          onClick={(e) => handleLogin(e)}
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
